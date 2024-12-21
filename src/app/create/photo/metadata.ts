db72524eec97fb35590f3485a2e2a3df3cca1d99
ridwano11p <ridwano11p@gmail.com>
2024-12-21 17:20:02 +0300

    photo create route added

---
+++ src/app/create/photo/metadata.ts
@@ -0,0 +1,28 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Upload Photo | Travel to End FGM",
+  description: "Upload new photos to the gallery section of Travel to End FGM.",
+  openGraph: {
+    title: "Upload Photo | Travel to End FGM",
+    description: "Upload new photos to the gallery section of Travel to End FGM.",
+    type: "website",
+    locale: "en_US",
+    siteName: "Travel to End FGM"
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Upload Photo | Travel to End FGM",
+    description: "Upload new photos to the gallery section of Travel to End FGM."
+  },
+  robots: {
+    index: false,
+    follow: false,
+    nocache: true,
+    googleBot: {
+      index: false,
+      follow: false,
+      noimageindex: true
+    }
+  }
+};
